d180bc67e4a28ec664d0a2239a0f60d9f3b25618
anousha21 <96038206+anousha21@users.noreply.github.com>
2022-01-30 18:03:09 +0500

    Update main.py

--- FYP_Coding_Task/main.py
+++ FYP_Coding_Task/main.py
@@ -39,6 +39,26 @@ model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentro
 model.summary()
 
 
+# Part 2 - Fitting the CNN to the images
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(
+    rescale=1. / 255)
+
+training_set = train_datagen.flow_from_directory(
+    'dataset/training_set',
+    target_size=(64, 64),
+    batch_size=12,
+    class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+    'dataset/test_set',
+    target_size=(64, 64),
+    batch_size=12,
+    class_mode='binary')
 
 # Part 3 - If the CNN weight model already exists make predictions
 if os.path.isfile("weights.h5") & os.path.isfile("model.h5"):

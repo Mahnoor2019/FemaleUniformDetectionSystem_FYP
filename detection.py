b95814865f2bae9cdeef35ae588515ff29499c8b
Mahnoor2019 <96038127+Mahnoor2019@users.noreply.github.com>
2022-05-30 11:41:15 +0500

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from PIL import Image
+from keras import models
+
+model = models.load_model('model.h5')
+video = cv2.VideoCapture(0)
+
+while True:
+    _, frame = video.read()
+
+    im = Image.fromarray(frame, 'RGB')
+
+    im = im.resize((128, 128))
+    img_array = np.array(im)
+
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = int(model.predict(img_array)[0][0])
+
+    print(prediction)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    if prediction == 0:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.putText(frame,
+                    'No uniform detected',
+                    (50, 50),
+                    font, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+    else:
+        cv2.putText(frame,
+                    'Uniform detected',
+                    (50, 50),
+                    font, 1,
+                    (0, 255, 255),
+                    2,
+                    cv2.LINE_4)
+
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
---
+++ model_train.py
@@ -0,0 +1,63 @@
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras.preprocessing import image
+import numpy as np
+
+size = 128
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(size, size, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.0003), loss='binary_crossentropy', metrics=['acc'])
+
+model.summary()
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory('data/train', target_size=(size, size),
+                                                    batch_size=64, class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory('data/valid', target_size=(size, size),
+                                                              batch_size=64, class_mode='binary')
+# New Method
+# history = model.fit(
+#  train_generator,
+# validation_data=validation_generator,
+# epochs=20
+# )
+
+# Old Method
+
+model.fit_generator(train_generator, epochs=5, steps_per_epoch=63,
+                    validation_data=validation_generator, validation_steps=7, workers=4)
+
+model.save('model.h5')

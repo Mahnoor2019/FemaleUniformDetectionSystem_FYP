bba8e890b4d4c13d545477026f4784480a38ef45
Laraib72 <96038125+Laraib72@users.noreply.github.com>
2022-06-07 08:07:58 +0500

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -19,18 +19,6 @@ model.add(layers.MaxPooling2D(2, 2))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D(2, 2))
 
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D(2, 2))
-
-model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-
-model.compile(optimizer=optimizers.RMSprop(lr=0.0003), loss='binary_crossentropy', metrics=['acc'])
-
-model.summary()
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
